5ca901dd637619d06903c195ad7b57d6cf5a811d
manuelsilv <manuel-silva@live.nl>
2019-07-22 11:51:54 +0200

    status updat

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,82 @@
-function deleteItem(e){
-
-}
-
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
 window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+  var calculatePriceButton = document.getElementsByClassName('calc-prices-button')[0];
+  var createItemButton = document.getElementsByClassName('new-item-create')[0];
+  
 
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  calculatePriceButton.addEventListener("click", calculateSubtotals);
+  addEventListenersToDeleteButtons("click", deleteRowFunction);
+  //debugger
+  createItemButton.addEventListener("click", createNewProduct);
 
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
+  function calculateSubtotals() {
+    let productRows = document.getElementsByClassName("product-wrap")
+    for(let i = 0; i < productRows.length; i++) {
+        calcSubTotal(productRows[i])
+    }
+    var allSubTotalsElements = document.getElementsByClassName("sub-total")
+    
+    let grandTotal = 0
+    for (let i = 0; i < allSubTotalsElements.length; i++){
+        grandTotal += parseInt(allSubTotalsElements[i].innerHTML)
+    }
+    document.getElementsByClassName("total-price")[0].innerHTML = grandTotal
   }
-};
+
+  function addEventListenersToDeleteButtons(){
+    var deleteButton = document.getElementsByClassName('delete-button')[0];
+    //debugger;
+    let deleteButtonsArray = document.getElementsByClassName("delete-button")
+    //deleteButton.addEventListener("click", deleteSubtotal);
+    for(let i = 0; i < deleteButtonsArray.length; i++) {
+      if(deleteButtonsArray.length > 0 ){
+        //deleteButtonsArray[i].addEventListener("click", (event)=>{deleteRowFunction(event)});
+        deleteButtonsArray[i].addEventListener("click", deleteRowFunction);
+      }
+    }
+    debugger;
+  }   
+  function deleteRowFunction(event){
+    //debugger;
+    // let element = document.getElementsByClassName('product-wrap')[0];
+    let element = this.parentNode.parentNode
+
+    // let one = this
+    // let two = event
+    debugger;
+    element.parentNode.removeChild(element);
+    
+   }
+
+  function calcSubTotal(productRow) {
+    var quantity = productRow.getElementsByClassName("howMany")[0].value;
+    var price = productRow.getElementsByClassName("price")[0].innerHTML;
+    productRow.getElementsByClassName("sub-total")[0].innerHTML = parseInt(quantity) * parseInt(price)
+  }
+
+  function createNewProduct(){
+    let createWrap = document.getElementsByClassName('product-wrap')[0];
+    let newProductWrap = createWrap.cloneNode(true);
+    let productPrice = document.getElementById('price');
+    let productName = document.getElementById('')
+    createWrap.parentNode.appendChild(newProductWrap);
+     // document.getElementsByClass('container').appendChild('product-wrap');
+    
+     //deleteRowFunction(event)
+     addEventListenersToDeleteButtons()
+ debugger;
+   }
+}
+
+
+
+  
+
+
+
+
+  
+
+  
+
+
+  
